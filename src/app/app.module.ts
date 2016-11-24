bb93e36bfbd9d1e65770eea481eefd14f4c3a380
Blind Despair <blind.despair@gmail.com>
2016-11-24 21:59:55 +0200

    Form component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInFormComponent
   ],
   imports: [
     BrowserModule,
